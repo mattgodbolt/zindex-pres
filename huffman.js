45823dfe73d5e189878c4c7b4504be0bec513046
Matt Godbolt <matthew.godbolt@aquatic.com>
2022-10-15 11:06:37 -0500

    More wip

---
+++ huffman.js
@@ -0,0 +1,70 @@
+class Node {
+    constructor(count, token, children) {
+        this.count = count;
+        this.token = token;
+        this.children = children;
+        this.name = null;
+    }
+
+    enname(map, prefix) {
+        this.name = prefix;
+        if (this.children) {
+            this.children[0].enname(map, prefix + '0');
+            this.children[1].enname(map, prefix + '1');
+        } else {
+            map[this.token] = this.name;
+        }
+    }
+
+    describe() {
+        if (this.children)
+            return `${this.count}`;
+        return `'${this.token}' (${this.count})`;
+    }
+
+    toDag() {
+        return {
+            token: this.token === ' ' ? "' '" : this.token,
+            id: this.name,
+            children: this.children ? this.children.map(child => child.toDag()).reverse() : []
+        }
+    }
+}
+
+class Tree {
+    constructor(root) {
+        this.root = root;
+        this.codeToSymbol = {};
+        this.root.enname(this.codeToSymbol, "");
+    }
+
+    toDag() {
+        return this.root.toDag();
+    }
+}
+
+function stringToNodes(text) {
+    const nodes = {};
+    for (const char of text) {
+        if (!nodes[char]) {
+            nodes[char] = new Node(1, char, null);
+        } else {
+            nodes[char].count++;
+        }
+    }
+    return Object.values(nodes);
+}
+
+export function makeTree(text) {
+    let nodes = stringToNodes(text);
+    while (nodes.length > 1) {
+        nodes = nodes.sort((x, y) => x.count - y.count);
+        const leftChild = nodes.shift()
+        const rightChild = nodes.shift();
+        const totalCount = leftChild.count + rightChild.count;
+        console.log(`Combining ${leftChild.describe()} and ${rightChild.describe()} to make a new node with ${totalCount}`);
+        const newNode = new Node(totalCount, null, [leftChild, rightChild]);
+        nodes.unshift(newNode);
+    }
+    return new Tree(nodes[0]);
+}
